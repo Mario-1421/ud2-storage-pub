83d91eccc7dd3fbc93570dc99341f79d0a08f9d8
Mario <mariopv1111@gmail.com>
2024-11-28 20:05:53 +0100

    Practica CSV

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CsvController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HelloWorldController;
@@ -8,3 +9,5 @@ use App\Http\Controllers\JsonController;
 Route::apiResource('hello', HelloWorldController::class);
 
 Route::apiResource('json', JsonController::class);
+
+Route::apiResource('csv', CsvController::class);
